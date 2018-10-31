2c417f497527a7253dec38d670c64ab2084a7650
anuhya45 <sreeanuhya45@msitprogram.net>
2018-10-31 17:28:34 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/M1/1.1 Graph/GraphList.java
+++ Assignment/M1/1.1 Graph/GraphList.java
@@ -1,129 +1,107 @@
-public class GraphList implements Graph {
-    private static final String NEWLINE = System.getProperty("line.separator");
-
-    private final int V;
-    private int E;
+/**
+ * List of graphs.
+ */
+class GraphList implements Graph {
+    /**
+     * vertices.
+     */
+    private int vertices;
+    /**
+     * edges.
+     */
+    private int edge;
+    /**
+     * bag.
+     */
     private Bag<Integer>[] adj;
-
     /**
-     * Initializes an empty GraphList with {@code V} vertices and 0 edges.
-     * param V the number of vertices
+     * Constructs the object.
      *
-     * @param  V number of vertices
-     * @throws IllegalArgumentException if {@code V < 0}
+     * @param      v1    The v 1
      */
-    public GraphList(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
+    GraphList(final int v1) {
+        this.vertices = v1;
+        this.edge = 0;
+        this.adj = (Bag<Integer>[]) new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adj[i] = new Bag<Integer>();
         }
     }
     /**
-     * Returns the number of vertices in this GraphList.
+     * vertices.
      *
-     * @return the number of vertices in this GraphList
+     * @return     { description_of_the_return_value }
      */
-    public int V() {
-        return V;
+    public int vertices() {
+        return this.vertices;
     }
-
     /**
-     * Returns the number of edges in this GraphList.
+     * number of edges.
      *
-     * @return the number of edges in this GraphList
+     * @return     { description_of_the_return_value }
      */
-    public int E() {
-        return E;
+    public int edges() {
+        return this.edge;
     }
-
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-    }
-
     /**
-     * Adds the undirected edge v-w to this GraphList.
+     * Adds an edge.
      *
-     * @param  v one vertex in the edge
-     * @param  w the other vertex in the edge
-     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
      */
-    public void addEdge(int v, int w) {
-        // validateVertex(v);
-        // validateVertex(w);
-        E++;
-        adj[v].add(w);
-        adj[w].add(v);
+    public void addEdge(final int v, final int w) {
+        if (!hasEdge(v, w) && v != w) {
+            edge++;
+            adj[v].add(w);
+            adj[w].add(v);
+        }
     }
-    public boolean hasEdge(int v, int w) {
-        int count = 0;
-        for (int i : adj[v]) {
-            if (i == w) {
-                count += 1;
-                break;
-            }
-        }
-        for (int i : adj[w]) {
-            if (i == v) {
-                count += 1;
-                break;
-            }
-        }
-        if (count == 2) {
-            return true;
-        }
-        return false;
-    }
-
     /**
-     * Returns the vertices adjacent to vertex {@code v}.
+     * iterable.
      *
-     * @param  v the vertex
-     * @return the vertices adjacent to vertex {@code v}, as an iterable
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
-    public Iterable<Integer> adj(int v) {
-        validateVertex(v);
+    public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
-
     /**
-     * Returns the degree of vertex {@code v}.
+     * Determines if it has edge.
      *
-     * @param  v the vertex
-     * @return the degree of vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
-    public int degree(int v) {
-        validateVertex(v);
-        return adj[v].size();
-    }
-
-
-    /**
-     * Returns a string representation of this GraphList.
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
      *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
-     *         followed by the <em>V</em> adjacency lists
+     * @return     True if has edge, False otherwise.
      */
-    public String display(String[] data) {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges" + NEWLINE);
-        if (E > 0) {
-            for (int v = 0; v < V; v++) {
-                s.append(data[v] + ": ");
-                for (int w : adj[v]) {
-                    s.append(data[w] + " ");
-                }
-                s.append(NEWLINE);
+    public boolean hasEdge(final int v, final int w) {
+        for (int k : adj[v]) {
+            if (k == w) {
+                return true;
             }
+        } return false;
+    }
+    /**
+     * display.
+     *
+     * @param      data  The data
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String display(final String[] data) {
+        String s = "";
+        s += vertices + " vertices, " + edge + " edges" + '\n';
+        if (edge == 0) {
+            s += "No edges ";
         } else {
-            s.append("No edges");
+            for (int v = 0; v < vertices; v++) {
+                s += data[v] + ": ";
+                for (int w : adj[v]) {
+                    s += data[w] + " ";
+                }
+                s += '\n';
+            }
         }
-        return s.toString();
+        return s.substring(0, s.length() - 1);
     }
 }
