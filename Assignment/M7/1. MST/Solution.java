7184c3a9ad3c6a5677c5094eccf73a6ba57a3597
anuhya45 <sreeanuhya45@msitprogram.net>
2018-11-05 19:36:15 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignment/M7/1. MST/Solution.java
@@ -0,0 +1,219 @@
+import java.util.Scanner;
+
+class Edge implements Comparable<Edge> { 
+
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public Edge(int v1, int w1, double weight1) {
+        this.v = v1;
+        this.w = w1;
+        this.weight = weight1;
+    }
+
+    /**
+     * Returns the weight of this edge.
+     *
+     * @return the weight of this edge
+     */
+    public double weight() {
+        return weight;
+    }
+
+    /**
+     * Returns either endpoint of this edge.
+     *
+     * @return either endpoint of this edge
+     */
+    public int either() {
+        return v;
+    }
+
+    /**
+     * Returns the endpoint of this edge that is different from the given vertex.
+     *
+     * @param  vertex one endpoint of this edge
+     * @return the other endpoint of this edge
+     * @throws IllegalArgumentException if the vertex is not one of the
+     *         endpoints of this edge
+     */
+    public int other(int vertex) {
+        if (vertex == v) {
+        	return w;
+        }
+        else if (vertex == w) {
+        	return v;
+        }
+        else {
+        	throw new IllegalArgumentException("Illegal endpoint");
+        }
+    }
+
+    /**
+     * Compares two edges by weight.
+     * Note that {@code compareTo()} is not consistent with {@code equals()},
+     * which uses the reference equality implementation inherited from {@code Object}.
+     *
+     * @param  that the other edge
+     * @return a negative integer, zero, or positive integer depending on whether
+     *         the weight of this is less than, equal to, or greater than the
+     *         argument edge
+     */
+    @Override
+    public int compareTo(Edge that) {
+        return Double.compare(this.weight, that.weight);
+    }
+
+    /**
+     * Returns a string representation of this edge.
+     *
+     * @return a string representation of this edge
+     */
+    public String toString() {
+        return String.format("%d-%d %.5f", v, w, weight);
+    }
+}
+
+class EdgeWeightedGraph {
+    private final int V;
+    private int E;
+    private Bag<Edge>[] adj;
+
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Edge>();
+        }
+    }
+
+     /**
+     * Initializes a new edge-weighted graph that is a deep copy of {@code G}.
+     *
+     * @param  G the edge-weighted graph to copy
+     */
+    public EdgeWeightedGraph(EdgeWeightedGraph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<Edge> reverse = new Stack<Edge>();
+            for (Edge e : G.adj[v]) {
+                reverse.push(e);
+            }
+            for (Edge e : reverse) {
+                adj[v].add(e);
+            }
+        }
+    }
+
+    /**
+     * Returns the number of vertices in this edge-weighted graph.
+     *
+     * @return the number of vertices in this edge-weighted graph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this edge-weighted graph.
+     *
+     * @return the number of edges in this edge-weighted graph
+     */
+    public int E() {
+        return E;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the undirected edge {@code e} to this edge-weighted graph.
+     *
+     * @param  e the edge
+     * @throws IllegalArgumentException unless both endpoints are between {@code 0} and {@code V-1}
+     */
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
+
+    /**
+     * Returns the edges incident on vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the edges incident on vertex {@code v} as an Iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Edge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}               
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    /**
+     * Returns all edges in this edge-weighted graph.
+     * To iterate over the edges in this edge-weighted graph, use foreach notation:
+     * {@code for (Edge e : G.edges())}.
+     *
+     * @return all edges in this edge-weighted graph, as an iterable
+     */
+    public Iterable<Edge> edges() {
+        Bag<Edge> list = new Bag<Edge>();
+        for (int v = 0; v < V; v++) {
+            int selfLoops = 0;
+            for (Edge e : adj(v)) {
+                if (e.other(v) > v) {
+                    list.add(e);
+                }
+                // add only one copy of each self loop (self loops will be consecutive)
+                else if (e.other(v) == v) {
+                    if (selfLoops % 2 == 0) list.add(e);
+                    selfLoops++;
+                }
+            }
+        }
+        return list;
+    }
+}
+
+public final class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int vertices = Integer.parseInt(s.nextLine());
+		int edges = Integer.parseInt(s.nextLine());
+		EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
+		while (s.hasNext()) {
+			String[] tokens = s.nextLine().split(" ");
+			Edge edge = new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
+			ewg.addEdge(edge);
+		}
+		KruskalMST krus = new KruskalMST(ewg);
+		System.out.format("%.5f", krus.weight());
+	}
+}
