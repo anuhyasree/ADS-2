e8739f6815002289acff7ebf86b0c189e3b4d7df
anuhya45 <sreeanuhya45@msitprogram.net>
2018-11-10 11:32:31 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/M12/ADS-2_Exam-2/Solution.java
+++ Assignment/M12/ADS-2_Exam-2/Solution.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 /**.
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 	/**.
 	 * epmty constructor.
 	 */
-	Solution() {
+	private Solution() {
 		//empty constructor
 	}
-	/**
+	/**.
 	 * { function_description }
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		// Self loops are not allowed...
 		// Parallel Edges are allowed...
 		// Take the Graph input here...
@@ -25,12 +25,13 @@ public class Solution {
 		for (int i = 0; i < road; i++) {
 			String[] tokens = scan.nextLine().split(" ");
 			if (!tokens[0].equals(tokens[1])) {
-				Edge obj = new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),
+				Edge obj = new Edge(Integer.parseInt(tokens[0]),
+				                    Integer.parseInt(tokens[1]),
 				                    Integer.parseInt(tokens[2]));
 				list.addEdge(obj);
 			}
 		}
-		String caseToGo = null;
+		String caseToGo = scan.nextLine();
 		switch (caseToGo) {
 		case "Graph":
 			//Print the Graph Object.
@@ -38,15 +39,18 @@ public class Solution {
 			break;
 
 		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
+			// Handle the case of DirectedPaths,
+			// where two integers are given.
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
 			break;
 
 		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
+			// Handle the case of ViaPaths, where
+			// three integers are given.
+			// First is the source and second is
+			//  the via is the one where path should pass throuh.
 			// third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
@@ -55,6 +59,5 @@ public class Solution {
 		default:
 			break;
 		}
-
 	}
 }
