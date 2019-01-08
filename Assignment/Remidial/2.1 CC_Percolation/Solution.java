b39866c206a6acf461a7bbbd335465a5b735e51a
anuhya45 <sreeanuhya45@msitprogram.net>
2019-01-08 11:35:42 +0530

    "ads2_m2_a1_1: 1/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/Remidial/2.1 CC_Percolation/Solution.java
+++ Assignment/Remidial/2.1 CC_Percolation/Solution.java
@@ -1,6 +1,6 @@
 import  java.util.*;
 
-class percolation{
+class percolation {
 
 	public int vertex;
 
@@ -10,55 +10,57 @@ class percolation{
 
 	public boolean connected(boolean[][] grid, Graph graph) {
 		int value = 0;
-		for (int i = 0; i< vertex; i++ ) {
-			for (int j = 0; j< vertex; j++ ) {
+		for (int i = 0; i < vertex; i++ ) {
+			for (int j = 0; j < vertex; j++ ) {
 				if (grid[i][j]) {
-					value = combine(i,j);
+					value = combine(i, j);
 				}
-				if (i==0) {
-					graph.addEdge(value,vertex*vertex);
+				if (i == 0) {
+					graph.addEdge(value, vertex * vertex);
 				}
-				if (i==vertex-1) {
-					graph.addEdge(value,vertex*vertex);
+				if (i == vertex - 1) {
+					graph.addEdge(value, vertex * vertex);
 				}
-				if (i-1 >= 0 && grid[i-1][j]) {
-					graph.addEdge(value,combine(i-1,j));
+				if (i - 1 >= 0 && grid[i - 1][j]) {
+					graph.addEdge(value, combine(i - 1, j));
 
 				}
-				if (i+1 < vertex && grid[i+1][j]) {
-					graph.addEdge(value,combine(i+1,j));
+				if (i + 1 < vertex && grid[i + 1][j]) {
+					graph.addEdge(value, combine(i + 1, j));
 				}
-
+				if (i + 1 < vertex && grid[i][j+j]) {
+					graph.addEdge(value, combine(i, j + 1));
+				}
 			}
 
 		}
-		DepthFirstPath depth = new DepthFirstPath(graph, vertex*vertex);
-		return depth.hasPathTo(vertex*vertex+1);
+		DepthFirstPath depth = new DepthFirstPath(graph, vertex * vertex);
+		return depth.hasPathTo(vertex * vertex + 1);
 
 	}
-	public int combine(int row, int col){
-	return((row*vertex)+col);
-}
+	public int combine(int row, int col) {
+		return ((row * vertex) + col);
+	}
 }
 
-public class Solution{
+public class Solution {
 	public static void main(String[] args) {
-	    Scanner input = new Scanner(System.in);
-	    int vert = input.nextInt();
-	    percolation perc = new percolation(vert);
-	    boolean[][] grid = new boolean[vert][vert];
-	    Graph graph = new Graph(vert*vert+2);
-	    try {
-	    	while (true){
-	    	int row = input.nextInt();
-	    	int col = input.nextInt();
-	    	grid[row-1][col-1]=true;
-	    }
-	} catch(Exception e) {
+		Scanner input = new Scanner(System.in);
+		int vert = input.nextInt();
+		percolation perc = new percolation(vert);
+		boolean[][] grid = new boolean[vert][vert];
+		Graph graph = new Graph(vert * vert + 2);
+		try {
+			while (true) {
+				int row = input.nextInt();
+				int col = input.nextInt();
+				grid[row - 1][col - 1] = true;
+			}
+		} catch (Exception e) {
 
-	}finally{
-		System.out.println(perc.connected(grid,graph));
-	}
+		} finally {
+			System.out.println(perc.connected(grid, graph));
+		}
 
 	}
 }
