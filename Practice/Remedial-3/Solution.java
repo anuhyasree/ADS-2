f393433cf5bdb9181c2de84c713af2e708d3b829
anuhya45 <sreeanuhya45@msitprogram.net>
2019-02-08 13:29:50 +0530

    "ads1_remedial_3_a1_1: 7/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Practice/Remedial-3/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		BinaryST<Student, Double> studentInput = new BinaryST();
+		for (int i = 0; i < n; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			studentInput.put(new Student(tokens[1], Double.parseDouble(tokens[2]),
+                Integer.parseInt(tokens[0])),
+                Double.parseDouble(tokens[2]));
+		}
+		int q = Integer.parseInt(scan.nextLine());
+		Iterable<Student> studentList = studentInput.keys();
+
+		for (int i = 0; i < q; i++) {
+			double marks = Double.parseDouble(scan.nextLine());
+			boolean check  = true;
+			for (Student stud: studentList) {
+
+			if (studentInput.get(stud) == marks) {
+				check = false;
+				// innerobj.add(s);
+				// obj.add(innerobj);
+
+				System.out.println(stud);
+			}
+		}
+		if (check) {
+			System.out.println("This marks are not awarded to any student");
+		 }
+	}
+	}
+}
