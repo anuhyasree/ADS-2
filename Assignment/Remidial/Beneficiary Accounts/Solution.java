1f2f7b9009845b1a54114e25744cdd6649852616
anuhya45 <sreeanuhya45@msitprogram.net>
2019-02-23 15:55:52 +0530

    "ads2_remedial_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignment/Remidial/Beneficiary Accounts/Solution.java
+++ Assignment/Remidial/Beneficiary Accounts/Solution.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 class Solution {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int a = Integer.parseInt(input.nextLine());
-		int b = Integer.parseInt(input.nextLine());
-		Graph di = new Graph(a);
-		while (input.hasNext()) {
-			String strg = input.nextLine();
-			String[] values = strg.split(" ");
-			di.addEdge(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
-		}
-		Cycle cy = new Cycle(di);
-		if (cy.hasCycle()) {
-			System.out.println("true");
-		} else{
-			System.out.println("No Self Beneficiaries.");
-		}
-	}
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int a = Integer.parseInt(input.nextLine());
+    int b = Integer.parseInt(input.nextLine());
+    Digraph di = new Digraph(a);
+    while (input.hasNext()) {
+      String strg = input.nextLine();
+      String[] values = strg.split(" ");
+      di.addEdge(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
+    }
+    DirectedCycle cy = new DirectedCycle(di);
+    if (cy.hasCycle()) {
+      System.out.println("true");
+    } else{
+      System.out.println("No Self Beneficiaries.");
+    }
+  }
 }
 
