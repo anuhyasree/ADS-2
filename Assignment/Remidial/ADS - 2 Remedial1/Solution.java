53bbd6197fb39b381ce7b3b5e58a25047daa3d0e
anuhya45 <sreeanuhya45@msitprogram.net>
2019-06-21 20:41:12 +0530

    "ads2_remedial_3_a1_1: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignment/Remidial/ADS - 2 Remedial1/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		int q = Integer.parseInt(input);
+		int s = Integer.parseInt(input);
+		Graph g = new Graph(q);
+		for (int i = 0; i < q ; i++ ) {
+			String[] str = scan.nextLine().split(" ");
+			g.addEdge(Integer.parseInt(str[0]),Integer.parseInt(str[1]));
+
+		}
+		int n = 1, count = 0, flag1 = 0, flag2 = 0;
+		for (int each: g.adj(q)) {
+			if (g.degree(each)<n+1) {
+				if (count < g.V()) {
+					count = g.degree(each);
+					flag1 = each;
+				}else if (count == g.degree(each)) {
+					flag2 = each;
+					count+= g.degree(each);
+				}
+			}
+		}
+
+	}
+}
